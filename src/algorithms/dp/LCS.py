ad0c9b4fe2391f52e82d21d9b36c0492589606ec
bk-anupam <bk_anupam@outlook.com>
2019-12-10 16:02:03 +0530

    Longest common subsequence implementation cut2, to find one of the common subsequence

--- src/algorithms/dp/LCS.py
+++ src/algorithms/dp/LCS.py
@@ -2,7 +2,7 @@ import sys
 
 
 # Bottom up approach by filling up a 2-D array
-def lcs_length(strx, stry):
+def lcs_table(strx, stry):
     lcs_array = [[0 for row in range(len(stry)+1)] for col in range(len(strx)+1)]
     for x in range(1, len(strx)+1):
         for y in range(1, len(stry)+1):
@@ -11,14 +11,30 @@ def lcs_length(strx, stry):
                 lcs_array[x][y] = lcs_array[x-1][y-1] + 1
             else:
                 lcs_array[x][y] = max(lcs_array[x][y-1], lcs_array[x-1][y])
-    val = lcs_array[len(strx)][len(stry)]
-    return val
+    return lcs_array
+
+
+# From the table, backtrack to retrieve one of the LCSes
+def backtrack(lcs_array, str_x, str_y, i, j):
+    if i == 0 or j == 0:
+        return ""
+    if str_x[i-1] == str_y[j-1]:
+        return backtrack(lcs_array, str_x, str_y, i-1, j-1) + str_x[i-1]
+    if lcs_array[i-1][j] > lcs_array[i][j-1]:
+        return backtrack(lcs_array, str_x, str_y, i-1, j)
+    else:
+        return backtrack(lcs_array, str_x, str_y, i, j-1)
 
 
 def main():
     if len(sys.argv) == 1:
         raise Exception("No arguments were provided. Provide the two strings to compare as argument")
-    common_string_len = lcs_length(sys.argv[1], sys.argv[2])
+    str_x = sys.argv[1]
+    str_y = sys.argv[2]
+    lcs_array = lcs_table(str_x, str_y)
+    common_string_len = lcs_array[len(str_x)][len(str_y)]
+    lcs = backtrack(lcs_array, str_x, str_y, len(str_x), len(str_y))
+    print('lcs: ' + lcs)
     print("Length of common subsequence: {}".format(common_string_len))
 
 
