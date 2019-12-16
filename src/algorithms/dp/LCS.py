6d4a32a67893bfc710c7f98293089233a157ca3f
bk-anupam <bk_anupam@outlook.com>
2019-12-16 12:40:53 +0530

    Longest common subsequence implementation cut3, to find all of the common subsequences

--- src/algorithms/dp/LCS.py
+++ src/algorithms/dp/LCS.py
@@ -26,6 +26,20 @@ def backtrack(lcs_array, str_x, str_y, i, j):
         return backtrack(lcs_array, str_x, str_y, i, j-1)
 
 
+def backtrack_all(lcs_array, str_x, str_y, i, j):
+    if i == 0 or j == 0:
+        return set([""])
+    elif str_x[i-1] == str_y[j-1]:
+        return set([lcs+str_x[i-1] for lcs in backtrack_all(lcs_array, str_x, str_y, i-1, j-1)])
+    else:
+        all_lcs = set()
+        if lcs_array[i-1][j] >= lcs_array[i][j-1]:
+            all_lcs.update(backtrack_all(lcs_array, str_x, str_y, i-1, j))
+        if lcs_array[i][j-1] >= lcs_array[i-1][j]:
+            all_lcs.update(backtrack_all(lcs_array, str_x, str_y, i, j-1))
+        return all_lcs
+
+
 def main():
     if len(sys.argv) == 1:
         raise Exception("No arguments were provided. Provide the two strings to compare as argument")
@@ -34,8 +48,11 @@ def main():
     lcs_array = lcs_table(str_x, str_y)
     common_string_len = lcs_array[len(str_x)][len(str_y)]
     lcs = backtrack(lcs_array, str_x, str_y, len(str_x), len(str_y))
-    print('lcs: ' + lcs)
+    all_lcs = backtrack_all(lcs_array, str_x, str_y, len(str_x), len(str_y))
     print("Length of common subsequence: {}".format(common_string_len))
+    print('one of the lcs: ' + lcs)
+    # test strings: AATCC, ACACG
+    print(all_lcs)
 
 
 if __name__ == "__main__":
