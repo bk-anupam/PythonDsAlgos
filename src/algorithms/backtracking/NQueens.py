3de34c7554a8ac9b379370bfae8cbb88e8897cbd
bk-anupam <bk_anupam@outlook.com>
2021-03-16 19:21:00 +0530

    Remove unwanted function

--- src/algorithms/backtracking/NQueens.py
+++ src/algorithms/backtracking/NQueens.py
@@ -37,20 +37,6 @@ def get_diagonal_indices(i, j, N):
     return diag_indices
 
 
-def is_attacked(queens_placed, p_row_index, p_col_index, N):
-    for q_row_index, q_col_index in queens_placed:
-        # The next queen can't be placed in the same row or column as an existing queen
-        if q_row_index == p_row_index or q_col_index == p_col_index:
-            return True
-        # Two diagonals can possibly pass thru each board cell. The next queen can't be placed
-        # in any of the cells of the two diagonals that pass thru an existing queen
-        q_diag_indices = get_diagonal_indices(q_row_index, q_col_index, N)
-        for index_pair in q_diag_indices:
-            if index_pair[0] == p_row_index and index_pair[1] == p_col_index:
-                return True
-    return False
-
-
 def is_cell_attacked(board, row_i, col_i, N):
     # Criteria 1: A queen already placed in row_i or col_i
     for col_index in range(N):
